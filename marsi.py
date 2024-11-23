ee7e55dddd0f801b635cb3df8a2c475650e8f0b9
angelaangelaa <demanenkoanzela640@gmail.com>
2024-11-23 14:02:08 +0600

    Update marsi.py
    second. add text in the last commit

--- marsi.py
+++ marsi.py
@@ -4,3 +4,14 @@ locations = [random.randint(1, 10) for _ in range(3)]
 weights = [300, 250, 163]
 
 print(".")
+
+while True:
+    guesses = [] 
+    for i in range(3):
+        guess = int(input())
+        guesses.append(guess)
+    if guesses == locations:
+        print()
+        break
+    else:
+        print()
