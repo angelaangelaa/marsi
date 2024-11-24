99f91e89c26acb35288d6994e8e37abd5f1e6899
angelaangelaa <demanenkoanzela640@gmail.com>
2024-11-24 11:19:50 +0600

    Update marsi.py
    added texts to inputs

--- marsi.py
+++ marsi.py
@@ -1,24 +1,24 @@
 import random
 
-locations = [random.randint(1, 10) for _ in range(3)]
-weights = [300, 250, 163]
+locations = [random.randint(1, 7) for _ in range(3)]
+weights = [400, 200, 113]
 
-print(".")
+print("Hello! Try to guess the correct locations of boxes. You need to guess three numbers between 1 and 7.")
 
 while True:
     guesses = [] 
     for i in range(3):
-        guess = int(input())
+        guess = int(input(f"Enter the location of box {i + 1} (1-7 km): "))
         guesses.append(guess)
         
     if guesses == locations:
         if sum(weights) == 713:
-            print()
-            print("locations")
+            print("Congratulations! You've found all the boxes.")
+            print(f"Locations: {locations}, weights: {weights}")
             break
         else:
-            print()
+            print("Incorrect weight! Try again.")
         
     else:
-        print()
-        locations = [random.randint(1, 10) for _ in range(3)]
+        print("Sorry, your guesses were wrong! The boxes were moved.")
+        locations = [random.randint(1, 7) for _ in range(3)]
