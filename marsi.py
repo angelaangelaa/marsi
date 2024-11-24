7943becb4848d7aede7b0e47a66e5ffebcb1036b
angelaangelaa <demanenkoanzela640@gmail.com>
2024-11-24 15:49:08 +0600

    Update marsi.py
    final changes, added correct guesses system

--- marsi.py
+++ marsi.py
@@ -7,9 +7,20 @@ print("Hello! Try to guess the correct locations of boxes. You need to guess thr
 
 while True:
     guesses = [] 
+    correct_guesses = 0
+    
     for i in range(3):
-        guess = int(input(f"Enter the location of box {i + 1} (1-7 km): "))
-        guesses.append(guess)
+        while True:
+            guess = input(f"Enter the location of box {i + 1} (1-7 km): ")
+            
+            if 1 <= guess <= 7 and guess.isdigit():
+                guesses.append(guess)
+                break
+            else:
+                print("Sorry, your input is incorrect. Enter a number between 1 and 7")
+    
+        if guesses[-1] == locations[i]:
+                correct_guesses += 1
         
     if guesses == locations:
         if sum(weights) == 713:
@@ -20,5 +31,5 @@ while True:
             print("Incorrect weight! Try again.")
         
     else:
-        print("Sorry, your guesses were wrong! The boxes were moved.")
+        print(f"Sorry, your guesses were wrong! The boxes were moved. Correct guesses so far: {correct_guesses}")
         locations = [random.randint(1, 7) for _ in range(3)]
